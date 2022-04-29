e1f8b8ce406ca5910ab93912ed8e4dbdac11f919
chaeyoung L <codud9028@naver.com>
2022-04-29 23:35:46 +0900

    Chapter3. 그리디

--- ICY/2_week/3-1. 거스름돈.py
+++ ICY/2_week/3-1. 거스름돈.py
@@ -2,7 +2,7 @@
 N = int(input("거슬러 줘야 할 돈을 입력해주세요."))
 num=0
 
-coin_type = [500,100,50,10] # 큰 단위 거스름돈 순으로
+coin_type = [500,100,50,10] # 거스름돈 큰 수부터 차례대로
 for i in coin_type:
     num += N//i
     N=N%i
